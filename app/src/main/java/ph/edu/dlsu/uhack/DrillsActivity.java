c01917e8377f8686e5877d60c05d31d0eaf08a9b
isakyaki <pamela_ortiz@dlsu.edu.ph>
2018-06-30 03:42:48 +0800

    Added OnClickListeners to each image button

--- app/src/main/java/ph/edu/dlsu/uhack/DrillsActivity.java
+++ app/src/main/java/ph/edu/dlsu/uhack/DrillsActivity.java
@@ -1,15 +1,12 @@
 package ph.edu.dlsu.uhack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class DrillsActivity extends AppCompatActivity {
-
-
-
-
     ImageButton ibTyphoon, ibFlood, ibEarthquake, ibFire;
 
     @Override
@@ -25,28 +22,28 @@ public class DrillsActivity extends AppCompatActivity {
         ibTyphoon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setContentView(R.layout.typhoon_screen);
             }
         });
 
         ibFlood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setContentView(R.layout.flood_screen);
             }
         });
 
         ibEarthquake.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setContentView(R.layout.earthquake_screen);
             }
         });
 
         ibFire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setContentView(R.layout.fire_screen);
             }
         });
     }
