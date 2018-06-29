a45c59cbf1ae936808db5b15bb0c3816ddb24dd5
Cheska <cheska_alejandrino@dlsu.edu.ph>
2018-06-30 02:03:47 +0800

    Added SMS Sending Feature

---
+++ app/src/main/java/ph/edu/dlsu/uhack/SOSMessage.java
@@ -0,0 +1,17 @@
+package ph.edu.dlsu.uhack;
+
+public class SOSMessage {
+    String message;
+
+    public SOSMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
