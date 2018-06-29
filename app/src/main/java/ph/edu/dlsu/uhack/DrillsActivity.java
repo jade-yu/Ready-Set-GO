6d6efe7bc3e6c3754b15bdfe6921a099d234c02e
isakyaki <pamela_ortiz@dlsu.edu.ph>
2018-06-30 02:19:24 +0800

    Fixed mistake of putting the DrillsActivity in the EvacuationAreas Activity

--- app/src/main/java/ph/edu/dlsu/uhack/DrillsActivity.java
+++ app/src/main/java/ph/edu/dlsu/uhack/DrillsActivity.java
@@ -2,12 +2,52 @@ package ph.edu.dlsu.uhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DrillsActivity extends AppCompatActivity {
+
+
+
+
+    ImageButton ibTyphoon, ibFlood, ibEarthquake, ibFire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drills);
+
+        ibTyphoon = findViewById(R.id.ib_typhoon);
+        ibFlood = findViewById(R.id.ib_flood);
+        ibEarthquake = findViewById(R.id.ib_earthquake);
+        ibFire = findViewById(R.id.ib_fire);
+
+        ibTyphoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ibFlood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ibEarthquake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ibFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/ph/edu/dlsu/uhack/EvacuationAreasActivity.java
+++ app/src/main/java/ph/edu/dlsu/uhack/EvacuationAreasActivity.java
@@ -2,8 +2,13 @@ package ph.edu.dlsu.uhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class EvacuationAreasActivity extends AppCompatActivity {
+
+
+    ImageButton ibTyphoon, ibFlood, ibEarthquake, ibFire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
