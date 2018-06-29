bbc5c664435f14647381f7e68563e67db75775ef
isakyaki <pamela_ortiz@dlsu.edu.ph>
2018-06-30 06:36:22 +0800

    Added back button in drills

--- app/src/main/java/ph/edu/dlsu/uhack/BurnActivity.java
+++ app/src/main/java/ph/edu/dlsu/uhack/BurnActivity.java
@@ -9,5 +9,7 @@ public class BurnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_burn);
+
+
     }
 }
--- app/src/main/java/ph/edu/dlsu/uhack/TyphoonActivity.java
+++ app/src/main/java/ph/edu/dlsu/uhack/TyphoonActivity.java
@@ -2,12 +2,25 @@ package ph.edu.dlsu.uhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class TyphoonActivity extends AppCompatActivity {
+
+    ImageButton ibBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_typhoon);
+
+        ibBack = findViewById(R.id.btn_back);
+
+        ibBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
