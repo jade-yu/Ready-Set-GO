1983b46a33c576d7944acac6b915616616ba4759
franchong <franchesca_chong@yahoo.com>
2018-06-30 04:39:02 +0800

    Added SOS button

--- app/src/main/java/ph/edu/dlsu/uhack/SettingsActivity.java
+++ app/src/main/java/ph/edu/dlsu/uhack/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,11 +14,21 @@ public class SettingsActivity extends AppCompatActivity {
     ImageView ivBack, ivContacts, ivMessage;
     RelativeLayout rlContacts, rlMessage;
     TextView tvSelected;
+    Button btnSOS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        btnSOS = findViewById(R.id.btn_sos);
+        btnSOS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_CALL);
+                i.setClass(getBaseContext(), SOSMessageActivity.class);
+            }
+        });
 
         ivBack = findViewById(R.id.iv_back);
         ivBack.setOnClickListener(new View.OnClickListener() {
