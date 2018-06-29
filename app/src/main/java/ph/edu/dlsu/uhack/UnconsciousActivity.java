2f298009b2a7a44529d767be826c86083a596717
isakyaki <pamela_ortiz@dlsu.edu.ph>
2018-06-30 06:45:55 +0800

    Added back button in burn and unconscious layout

--- app/src/main/java/ph/edu/dlsu/uhack/UnconsciousActivity.java
+++ app/src/main/java/ph/edu/dlsu/uhack/UnconsciousActivity.java
@@ -9,5 +9,7 @@ public class UnconsciousActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unconscious);
+
+
     }
 }
