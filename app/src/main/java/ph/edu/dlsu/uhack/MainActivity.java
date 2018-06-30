62973964f27146940656ca55b403bf331058d359
jade-yu <jade_yu@dlsu.edu.ph>
2018-06-30 12:40:55 +0800

    sms function

--- app/src/main/java/ph/edu/dlsu/uhack/MainActivity.java
+++ app/src/main/java/ph/edu/dlsu/uhack/MainActivity.java
@@ -1,16 +1,28 @@
 package ph.edu.dlsu.uhack;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnTest;
+    DatabaseHelper dbHelper;
+    Button btnSOS;
+    String msg = "THIS IS AN EMERGENCY! THIS IS MY LOCATION ... SEND HELP!";
+
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    String number = "09176273602";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +53,63 @@ public class MainActivity extends AppCompatActivity {
         pbGo.setMax(100);
         pbGo.setProgressDrawable(drawable3);
 
-        btnTest = findViewById(R.id.btntest);
-        btnTest.setOnClickListener(new View.OnClickListener() {
+
+        btnSOS = (Button) findViewById(R.id.btn_sos);
+        btnSOS.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // placeholder only
-                // TODO actual implementation
-                pbReady.incrementProgressBy(5);
-                pbSet.incrementProgressBy(10);
-                pbGo.incrementProgressBy(15);
+            public void onClick(View view) {
+                Log.d("buttonClicked ", "Click");
+                sendSMSMessage();
             }
         });
+    }
+
+    protected void sendSMSMessage() {
+        Log.d("buttonClicked ", "sendSMSMessage");
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS) !=
+                PackageManager.PERMISSION_GRANTED) {
+            Log.d("TAG", "Permission not granted");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+            Log.d("TAG", "ACCEPTED");
+        } else {
+            Log.d("TAG","HERe");
+            sendAgain();
+        }
+    }
+
+    public void sendAgain() {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(number, null, msg, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e ) {
+            Toast.makeText(getApplicationContext(), "SMS failed to send.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        String test = Integer.toString(MY_PERMISSIONS_REQUEST_SEND_SMS);
+        Log.d("TAG",test);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (permissions[0].equalsIgnoreCase(Manifest.permission.SEND_SMS)
+                        && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    sendAgain();
+                } else {
+                    // Permission denied.
+                    Log.d("TAG", "FAILED PERMISSION");
+                    Toast.makeText(this,"FAILED PERMISSION",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
 }
